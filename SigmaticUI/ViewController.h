6ec571aa31fa383629cf9a4a3e0f15d978949f3a
hisham <hishamabri@gmail.com>
2015-06-17 15:38:49 +1000

    Project initial commit

---
+++ SigmaticUI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SigmaticUI
+//
+//  Created by Hisham on 17/06/2015.
+//  Copyright (c) 2015 Sigmatic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SigmaticUI/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SigmaticUI
+//
+//  Created by Hisham on 17/06/2015.
+//  Copyright (c) 2015 Sigmatic. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
